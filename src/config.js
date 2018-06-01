cfea26bfb84319b0194155d3441bf79928d59905
zmc <756343483@qq.com>
2018-06-01 11:19:23 +0800

    修复视频宽度问题

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const Config = {
-  API_ROOT: 'http://localhost:8083/'
-  /*API_ROOT: 'http://47.106.105.117:8083/'*/
+  /*API_ROOT: 'http://localhost:8083/'*/
+  API_ROOT: 'http://47.106.105.117:8083/'
 }
 
 module.exports = Config
